677c6700603daa2dfc267af2c10bceb7d5d61063
Tacro Hashimoto <tacro@g.ucla.edu>
2018-04-04 21:52:59 -0700

    enable carrierwave

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
-  # mount_base64_uploader :image_name, ItemImageUploader
+  mount_base64_uploader :image_name, ItemImageUploader
   # compromised uploading serial images to use base64...
   # mount_uploaders :image_name, ItemImageUploader
   # serialize :image_name, JSON
-  # attr_accessor :remote_image_url
+  attr_accessor :remote_image_url
   validates :image_name, {presence: true}
   validates :designer_id, {presence: true}
   # not deleting these 3 vars. They might be necessary when service has grown...
