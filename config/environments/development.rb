3d56594520ac32404a632ef8a0db12acf2dd5c3e
Tacro Hashimoto <tacro@g.ucla.edu>
2018-04-05 02:38:20 -0700

    new mail setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,15 +34,26 @@ Rails.application.configure do
   # SMTP Settings
   config.action_mailer.delivery_method = :smtp
 
+  # config.action_mailer.smtp_settings = {
+  # 	address:               'mail.gandi.net',
+  # 	port:      	           587,
+  # 	domain:                ENV['DOMAIN'],
+  # 	user_name:             ENV['USER_NAME_GANDI'],
+  # 	password:              ENV['PASSWORD_GANDI'],
+  # 	authentication:        'plain',
+  # 	enable_starttls_auto:  true
+  # }
+
   config.action_mailer.smtp_settings = {
-  	address:               'mail.gandi.net',
+  	address:               'smtp.gmail.com',
   	port:      	           587,
-  	domain:                ENV['DOMAIN'],
-  	user_name:             ENV['USER_NAME_GANDI'],
-  	password:              ENV['PASSWORD_GANDI'],
+  	domain:                'gmail.com',
+  	user_name:             ENV['USER_NAME_GMAIL'],
+  	password:              ENV['PASSWORD_GMAIL'],
   	authentication:        'plain',
   	enable_starttls_auto:  true
   }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
